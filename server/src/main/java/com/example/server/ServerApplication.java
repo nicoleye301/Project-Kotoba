9d28f77d2e51ed5707c6d2419f4f0fbfada6ca5f
FutabaRio28 <tony30428@gmail.com>
2025-01-13 06:26:48 -0500

    rename package Entity to entity

--- server/src/main/java/com/example/server/ServerApplication.java
+++ server/src/main/java/com/example/server/ServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.example.server.mapper")
-public class Application {
+public class ServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ServerApplication.class, args);
     }
 
 }
