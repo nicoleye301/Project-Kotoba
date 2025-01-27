7f07f0168b6807f90c210fa4de30ada5c944f76d
FutabaRio28 <tony30428@gmail.com>
2025-01-27 16:14:24 -0500

    changed from vue to react native

--- server/src/main/java/com/example/server/exception/GlobalExceptionHandler.java
+++ server/src/main/java/com/example/server/exception/GlobalExceptionHandler.java
@@ -4,12 +4,12 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
+//written by chatgpt
 @ControllerAdvice("com.example.springboot.controller")
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<String> handleCustomException(CustomException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatusCode.valueOf(ex.getCode()));  // 返回 400 错误和错误信息
+        return new ResponseEntity<>(ex.getMessage(), HttpStatusCode.valueOf(ex.getCode()));
     }
 }
