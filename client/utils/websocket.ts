fe7be71ab6078505e13b373778fd59f0e3ab0165
nicoleye301 <nicoleye301@gmail.com>
2025-03-07 08:36:12 -0500

    updated rollback/ fixed some issue

--- client/utils/websocket.ts
+++ client/utils/websocket.ts
@@ -1,41 +1,60 @@
-
 import eventEmitter from "@/utils/eventEmitter";
 
 const wsUrl = "ws://10.0.2.2:8080/ws";
-let socket;
+let socket: WebSocket | null = null;
 
-export function connect() {
+export function connect(): WebSocket {
+
+    if (socket &&
+        (socket.readyState === WebSocket.OPEN || socket.readyState === WebSocket.CONNECTING)
+    ) {
+        console.log("WebSocket already connected or connecting.");
+        return socket;
+    }
+
     // create the WebSocket connection - connection establish immediately
     socket = new WebSocket(wsUrl);
 
     // called when connection is opened
-    socket.onopen = function (event) {
+    socket.onopen = function (event: Event) {
         console.log("WebSocket is open now.");
     };
 
     // called when a message is received
-    socket.onmessage = function (event) {
+    socket.onmessage = function (event : MessageEvent) {
         console.log("Received:", event.data);
         // emit the message to event emitter so other parts of app can listen
         eventEmitter.emit("message", event.data);
     };
 
     // called when there is an error
-    socket.onerror = function (event) {
+    socket.onerror = function (event: Event) {
         console.error("WebSocket error observed:", event);
     };
 
     // called when connection is closed
     socket.onclose = function (event) {
         console.log("WebSocket is closed now.");
+        socket = null;
     };
+    return socket;
 }
 
-export function sendMessage(message) {
+//close websocket connection if exist
+export function close(): void {
+    if (socket) {
+        console.log("Closing WebSocket connection.");
+        socket.close();
+        socket = null;
+    }
+}
+
+
+export function sendMessage(message: string):void {
     if (socket && socket.readyState === WebSocket.OPEN) {
         socket.send(message);
     } else {
-        console.error("WebSocket is not open. Ready state: ", socket.readyState);
+        console.error("WebSocket is not open. Ready state: ", socket ? socket.readyState : "no socket");
     }
 }
 
