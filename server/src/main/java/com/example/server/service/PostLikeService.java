e8301bc3d37bcc305dfc6185b8c6ab3ac8c0de56
IsaiahBuddeau <isaiahbuddeau@gmail.com>
2025-04-21 19:18:31 -0400

    made it delete the entry in the table if they are unliking or undisliking the post

--- server/src/main/java/com/example/server/service/PostLikeService.java
+++ server/src/main/java/com/example/server/service/PostLikeService.java
@@ -25,6 +25,7 @@ public class PostLikeService {
             PostLike postLike;
             if(postLikeDatabase == null || postLikeDatabase.isEmpty())
             {
+                System.out.println("adding like to database");
                 postLike = new PostLike();
                 postLike.setPostId(postId);
                 postLike.setSenderId(senderId);
@@ -34,15 +35,19 @@ public class PostLikeService {
             else
             {
                 postLike = postLikeDatabase.get(0);
+                System.out.println("PostLikeDatabase: post id: " + postLikeDatabase.get(0).getPostId() + ", sender id: " + postLikeDatabase.get(0).getSenderId() + ", content: " + postLikeDatabase.get(0).getContent());
+                System.out.println("postLike: post id: " + postLike.getPostId() + ", sender id: " + postLike.getSenderId() + ", content: " + postLike.getContent());
                 if(postLikeDatabase.get(0).getContent().equals("like"))
                 {
-                    postLike.setContent("neutral");
+                    System.out.println("deleting like from database");
+                    postLikeMapper.deleteLikeByPostIdAndSenderId(postId, senderId);
                 }
                 else
                 {
+                    System.out.println("changing dislike to like in database");
                     postLike.setContent("like");
+                    postLikeMapper.updatePostLike(postLike);
                 }
-                postLikeMapper.updatePostLike(postLike);
             }
             return postLike;
         } catch(Exception e) {
@@ -56,6 +61,7 @@ public class PostLikeService {
             PostLike postLike;
             if(postLikeDatabase == null || postLikeDatabase.isEmpty())
             {
+                System.out.println("adding dislike to database");
                 postLike = new PostLike();
                 postLike.setPostId(postId);
                 postLike.setSenderId(senderId);
@@ -65,15 +71,20 @@ public class PostLikeService {
             else
             {
                 postLike = postLikeDatabase.get(0);
+                System.out.println("PostLikeDatabase: post id: " + postLikeDatabase.get(0).getPostId() + ", sender id: " + postLikeDatabase.get(0).getSenderId() + ", content: " + postLikeDatabase.get(0).getContent());
+                System.out.println("postLike: post id: " + postLike.getPostId() + ", sender id: " + postLike.getSenderId() + ", content: " + postLike.getContent());
+
                 if(postLikeDatabase.get(0).getContent().equals("dislike"))
                 {
-                    postLike.setContent("neutral");
+                    System.out.println("deleting dislike from database");
+                    postLikeMapper.deleteLikeByPostIdAndSenderId(postId, senderId);
                 }
                 else
                 {
+                    System.out.println("changing like to dislike in database");
                     postLike.setContent("dislike");
+                    postLikeMapper.updatePostLike(postLike);
                 }
-                postLikeMapper.updatePostLike(postLike);
             }
             return postLike;
         } catch(Exception e) {
