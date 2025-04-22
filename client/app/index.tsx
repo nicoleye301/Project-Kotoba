ddf7cefba423e10b0b2dbd2151e072cf9c325864
nicoleye301 <nicoleye301@gmail.com>
2025-04-22 07:03:27 -0400

    added firebase config

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -3,7 +3,7 @@ import { Platform } from "react-native";
 
 export default function Index() {
     if (Platform.OS === "android") {
-        // @ts-ignore – expo-navigation-bar is Android-only
+        // @ts-ignore
         import("expo-navigation-bar").then((NavigationBar) => {
             NavigationBar.setBackgroundColorAsync("white");
             NavigationBar.setButtonStyleAsync("dark");
